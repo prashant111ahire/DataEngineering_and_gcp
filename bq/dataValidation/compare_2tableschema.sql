0f87f9fe02c288b09b719ad9b5b317ddad692e59
prashant111ahire <141619680+prashant111ahire@users.noreply.github.com>
2025-03-07 12:53:29 -0800

    Update compare_2tableschema.sql

--- bq/dataValidation/compare_2tableschema.sql
+++ bq/dataValidation/compare_2tableschema.sql
@@ -1,31 +1,30 @@
--- Query to compare column names and data types of two tables
 WITH table_1_columns AS (
   SELECT 
-    column_name AS column_name_1,
+    field_path AS field_path_1,
     data_type AS data_type_1
-  FROM `jwn-nap-dataplex-prod-emue.region-us-west1.INFORMATION_SCHEMA.COLUMNS`
-  WHERE table_name = 'customer_activity_added_to_bag'
-  and table_schema='ACP_EVENT_VIEW'
+  FROM `<PROJECT_ID>.<REGION>.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
+  WHERE table_name = '<TABLE_NAME>'
+    AND table_schema = 'TABLE_SCHEMA'
 ),
 table_2_columns AS (
   SELECT 
-    column_name AS column_name_2,
+    field_path AS field_path_2,
     data_type AS data_type_2
-  FROM `jwn-nap-dataplex-nonprod-lpyu.region-us-west1.INFORMATION_SCHEMA.COLUMNS`
-  WHERE table_name = 'customer_activity_added_to_bag_parquet'
-  and table_schema='acp_event'
+  FROM `<PROJECT_ID>.<REGION>.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
+  WHERE table_name = '<TABLE_NAME>'
+    AND table_schema = 'TABLE_SCHEMA'
 )
 SELECT 
-  COALESCE(t1.column_name_1, t2.column_name_2) AS column_name,
+  COALESCE(t1.field_path_1, t2.field_path_2) AS field_path,
   t1.data_type_1 AS table_1_data_type,
   t2.data_type_2 AS table_2_data_type,
   CASE
-    WHEN t1.column_name_1 IS NULL THEN 'Missing in Table 1'
-    WHEN t2.column_name_2 IS NULL THEN 'Missing in Table 2'
-    WHEN t1.data_type_1 != t2.data_type_2 THEN 'Data type mismatch'
+    WHEN t1.field_path_1 IS NULL THEN 'Missing in Table 1'
+    WHEN t2.field_path_2 IS NULL THEN 'Missing in Table 2'
+    WHEN lower(t1.data_type_1) != lower(t2.data_type_2) THEN 'Data type mismatch'
     ELSE 'Match'
   END AS status
 FROM table_1_columns t1
 FULL OUTER JOIN table_2_columns t2
-ON LOWER(t1.column_name_1) = LOWER(t2.column_name_2) -- Case-insensitive comparison
-ORDER BY column_name;
+  ON LOWER(t1.field_path_1) = LOWER(t2.field_path_2)
+ORDER BY field_path;
